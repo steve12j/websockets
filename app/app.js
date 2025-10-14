6545652bfbf57c0d7bb8ebe9f5068fa65d39f7ef
steve jose <steve1999jose@gmail.com>
2025-10-14 18:23:15 +0530

    replace webscocket io with socketio

--- app/app.js
+++ app/app.js
@@ -1,15 +1,10 @@
-const socket = new WebSocket("ws://localhost:3000");
+const socket = io("ws://localhost:3500");
 
 const sendMessage = (e) => {
     e.preventDefault();
-    console.log(e);
     const input = document.querySelector("input");
     if(input.value){
-       if (socket.readyState === WebSocket.OPEN) {
-          socket.send(input.value);
-         } else {
-          console.warn("WebSocket is not open. Message not sent.");
-       }
+        socket.emit("message",input.value);
         input.value = "";
     }
     input.focus();
@@ -18,9 +13,8 @@ const sendMessage = (e) => {
 const form  = document.querySelector("form");
 form.addEventListener("submit", sendMessage)
 
-socket.addEventListener("message", ({data}) => {
-    const ul = document.querySelector("ul");
+socket.on("message", (data) => {
     const li = document.createElement("li");
     li.textContent = data;
-    ul.appendChild(li);
+    document.querySelector("ul").appendChild(li);
 })
--- server/index.js
+++ server/index.js
@@ -1,10 +1,20 @@
-const ws = require("ws");
-const server = new ws.Server({port: '3000'});
+import { createServer } from "http";
+import { Server } from "socket.io";
 
-server.on("connection", socket => {
-    socket.on("message", message => {
-        console.log(new Buffer.from(message).toString(), "Message");
-        socket.send(`${message}`);
+const httpServer =  createServer();
+
+const io = new Server(httpServer, {
+    cors : {
+        orgin : process.env.NODE_ENV === "production" ? false : ["http://localhost:5500", "http://127.0.0.1:5550"]
+    }
+})
+
+io.on("connection", socket => {
+    console.log(`User ${socket.id} socked connected`)
+    socket.on("message", data => {
+        io.emit("message",`${socket.id.substring(0,5)}: ${data}`);
     });
 });
+
+httpServer.listen(3500, () => console.log("listening to port 3500"));
 
