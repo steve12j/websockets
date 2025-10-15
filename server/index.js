5337c9864e4c76d982b27173e8e80880633203c0
steve jose <steve1999jose@gmail.com>
2025-10-15 10:57:30 +0530

    host front end with express in server

--- server/index.js
+++ server/index.js
@@ -1,13 +1,27 @@
-import { createServer } from "http";
 import { Server } from "socket.io";
+import path from "path";
+import express from "express";
+import { fileURLToPath } from "url"
 
-const httpServer =  createServer();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-const io = new Server(httpServer, {
+
+const PORT = process.env.PORT || 3500;
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+
+const expressServer = app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`)
+});
+
+const io = new Server(expressServer, {
     cors : {
         orgin : process.env.NODE_ENV === "production" ? false : ["http://localhost:5500", "http://127.0.0.1:5550"]
     }
-})
+});
 
 io.on("connection", socket => {
     console.log(`User ${socket.id} socked connected`)
@@ -16,5 +30,4 @@ io.on("connection", socket => {
     });
 });
 
-httpServer.listen(3500, () => console.log("listening to port 3500"));
 
